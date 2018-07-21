27144220fcb066eda70b70553d2779a5ad4e1c5d
三墨 <sanmo@juanpi.com>
2018-07-21 16:16:28 +0800

    feat(jbase): integer.

---
+++ jdk5/src/main/java/integer/Main.java
@@ -0,0 +1,22 @@
+package integer;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        /**
+         * JVM中一个字节以下的整型数据会在JVM启动的时候加载进内存，
+         * 除非用new Integer()显式的创建对象，否则都是同一个对象。
+         */
+
+        Integer i01 = 59;
+        int i02 = 59;
+        Integer i03 =Integer.valueOf(59);
+        Integer i04 = new Integer(59);
+
+        System.out.println(i03==i04);
+
+    }
+
+}
