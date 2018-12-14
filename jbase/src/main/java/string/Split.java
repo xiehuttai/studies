47d5b0678b64d3aef7f9f7fc5d2fec5e2778994f
sanmo <xiehu@globalegrow.com>
2018-12-14 15:23:33 +0800

    feat(all): 1.jdk=8 2.string split \n \u003d

---
+++ jbase/src/main/java/string/Split.java
@@ -0,0 +1,20 @@
+package string;
+
+import java.util.Arrays;
+
+public class Split {
+
+    private static final String STR="#日志配置\\nlog.root.appender\\u003dinfo,console,error,kafka,trace,debug\\nlog.root.level\u003derror\n";
+
+    public static void main(String[] args) {
+
+        System.out.println(STR);
+
+        String[] split = STR.split("\\\\n");
+        System.out.println(Arrays.asList(split));
+
+        String[] split1 = STR.split("\\\\u003d");
+        System.out.println(Arrays.asList(split1));
+    }
+
+}
