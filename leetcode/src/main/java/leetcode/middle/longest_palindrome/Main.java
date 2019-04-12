0435610caec50ce2efe506bac52a0d7ccee0539c
sanmo <285671269@qq.com>
2019-04-12 17:01:32 +0800

    leetcode middle one

--- leetcode/src/main/java/leetcode/middle/longest_palindrome/Main.java
+++ leetcode/src/main/java/leetcode/middle/longest_palindrome/Main.java
@@ -1,7 +1,7 @@
 package leetcode.middle.longest_palindrome;
 
 /**
- * 最大回文子串
+ * 最大回文子串，对问题要分析清楚
  */
 public class Main {
 
@@ -13,22 +13,12 @@ public class Main {
         // bb
         String str2 = "abbc";
 
+        // ccc
         String str3 = "ccc";
 
-        String str4 = "aaaa";
-
-        System.out.println(1);
         String result1 = new Main().longestPalindrome(str1);
-        System.out.println(result1);
-        System.out.println(2);
         String result2 = new Main().longestPalindrome(str2);
-        System.out.println(result2);
-        System.out.println(3);
         String result3 = new Main().longestPalindrome(str3);
-        System.out.println(result3);
-        System.out.println(4);
-        String result4 = new Main().longestPalindrome(str4);
-        System.out.println(result4);
 
         System.out.println();
 
@@ -81,28 +71,4 @@ public class Main {
         }
         return result;
     }
-
-    public String longestPalindromeOther(String s) {
-        int len = s.length();
-        while (len > 0) {
-            for (int i = 0; i < s.length() - len + 1; i++) {
-                if (isPalindrome(s, i, i + len - 1))
-                    return s.substring(i, len + i);
-            }
-            len--;
-        }
-        return "";
-    }
-
-    public boolean isPalindrome(String s, int start, int end) {
-        while (start <= end) {
-            if (s.charAt(start) != s.charAt(end)) {
-                return false;
-            }
-            start++;
-            end--;
-        }
-        return true;
-    }
-
 }
